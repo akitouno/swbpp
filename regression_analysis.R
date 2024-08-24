11c4a30d619522a7b46e2a8e7cb6475dc47e3e9d
akitouno <113277478+akitouno@users.noreply.github.com>
2024-08-24 20:41:23 +0900

    Update regression_analysis.R

--- regression_analysis.R
+++ regression_analysis.R
@@ -11,10 +11,10 @@ d1 <- read.csv('data1.csv')
 scale_numeric <- function(column) as.numeric(scale(column))
 
 numeric_vars <- c('income', 'education_mother', 'education_father', 'pubertal_stage', 'iq', 'aspiration', 
-                  'self_control', 'prosocial_behavior', 'sibling', 'grand_parent', 'life_satisfaction_of_caregiver', 
+                  'self_control', 'prosocial_behavior', 'sibling', 'grandparent', 'life_satisfaction_of_caregiver', 
                   'talk_with_caregiver', 'cooperative_spouse_in_childcare', 
                   'satisfied_with_family', 'want_to_be_like_mother', 'want_to_be_like_father',  
-                  'caregiver_interferes_of_future_work', 'caregiver_life_plan_is_stability_oriented',
+                  'family_involvement_in_future_carrer_preferences', 'caregiver_life_plan_is_stability_oriented',
                   'like_school', 'social_relationships_outside_school',
                   'good_neighborhood_for_childcare', 'neighborhood_cohesion_and_trust')
 d1[numeric_vars] <- lapply(d1[numeric_vars], scale_numeric)
@@ -34,10 +34,10 @@ selected_vars <- c('sex', 'primary_caregiver', 'ethnic_minority', 'income', 'edu
                    'pubertal_stage', 'iq', 'help_seeking_intention', 'aspiration', 'self_control', 'prosocial_behavior', 
                    'kindergarten_entrance_exam', 'elementary_school_entrance_exam', 'tutoring_school',
                    'chronic_health_problems', 'special_needs', 'overweight', 
-                   'separation_mother', 'separation_father', 'sibling', 'elder_sibling', 'grand_parent',  
+                   'separation_mother', 'separation_father', 'sibling', 'elder_sibling', 'grandparent',  
                    'smoker_in_household', 'life_satisfaction_of_caregiver', 'chronic_health_problems_mother', 'chronic_health_problems_father', 
                    'cooperative_spouse_in_childcare', 'satisfied_with_family', 'talk_with_caregiver', 
-                   'want_to_be_like_mother', 'want_to_be_like_father', 'caregiver_interferes_of_future_work', 'caregiver_life_plan_is_stability_oriented', 
+                   'want_to_be_like_mother', 'want_to_be_like_father', 'family_involvement_in_future_carrer_preferences', 'caregiver_life_plan_is_stability_oriented', 
                    'like_school', 'social_relationships_outside_school', 'park', 'neighborhood_with_friends_or_relatives', 
                    'good_neighborhood_for_childcare', 'neighborhood_cohesion_and_trust', 
                    'class_parallel_process_lcga')
@@ -56,7 +56,7 @@ imp_df_ref3 <- mice(df, m=100, maxit=50, seed=1234)
 ### Multinomial logistic regression analysis
 # Following variables were excluded: Primary caregiver, employment of mother/father, house ownership, help-seeking intention, park, and neighborhood with friends or relatives
 
-equation <- 'class_parallel_process_lcga~sex+ethnic_minority+income+education_mother+education_father+pubertal_stage+iq+aspiration+self_control+prosocial_behavior+kindergarten_entrance_exam+elementary_school_entrance_exam+tutoring_school+chronic_health_problems+special_needs+overweight+separation_mother+separation_father+sibling+elder_sibling+grand_parent+smoker_in_household+life_satisfaction_of_caregiver+chronic_health_problems_mother+chronic_health_problems_father+cooperative_spouse_in_childcare+satisfied_with_family+talk_with_caregiver+want_to_be_like_mother+want_to_be_like_father+caregiver_interferes_of_future_work+caregiver_life_plan_is_stability_oriented+like_school+social_relationships_outside_school+good_neighborhood_for_childcare+neighborhood_cohesion_and_trust'
+equation <- 'class_parallel_process_lcga~sex+ethnic_minority+income+education_mother+education_father+pubertal_stage+iq+aspiration+self_control+prosocial_behavior+kindergarten_entrance_exam+elementary_school_entrance_exam+tutoring_school+chronic_health_problems+special_needs+overweight+separation_mother+separation_father+sibling+elder_sibling+grandparent+smoker_in_household+life_satisfaction_of_caregiver+chronic_health_problems_mother+chronic_health_problems_father+cooperative_spouse_in_childcare+satisfied_with_family+talk_with_caregiver+want_to_be_like_mother+want_to_be_like_father+family_involvement_in_future_carrer_preferences+caregiver_life_plan_is_stability_oriented+like_school+social_relationships_outside_school+good_neighborhood_for_childcare+neighborhood_cohesion_and_trust'
 
 fit <-with(imp_df, nnet::multinom(as.formula(equation)))
 summary <- summary(pool(fit))
